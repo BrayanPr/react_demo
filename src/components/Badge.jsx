f0c6adfc4a372325e7085f6be9a436b552a2e4af
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 16:35:41 -0600

    Creating Badge Style

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -7,11 +7,11 @@ class Badge extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="Badge m-5 flex-comlumn">
+                <div className="Badge m-5">
                     <div className="Badge__header">
                         <img src={HeaderBackground} alt="Header Background" />
                     </div>
-                    <div className="container mt-5 mb1 Badge__userInfo">
+                    <div className="Badge__userInfo container mt-5 mb1 ">
                         <div className="Badge__userImage">
                             <img src={ProfilePic} alt="profile_pic" />
                         </div>
