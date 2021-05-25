100a75c5fe5d93329f5ede8d056c86e38c3f3e26
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 09:21:47 -0600

    adding Badge to the landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Layout from "./pages/Layout/Layout"
 import Main from "./pages/Main/Main"
+import Badge from "./components/Badge"
+
 function App() {
   return ( 
   <React.Fragment>
     <Layout>
       <Main></Main>
+      <Badge></Badge>
     </Layout>
   </React.Fragment>  
   );
