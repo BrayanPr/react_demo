6cf643d8dadb718b54c5445a03edc04a498f6aff
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 19:01:00 -0600

    adding elements to the new Badge

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -5,11 +5,40 @@ import Badge from "../../components/Badge"
 
 class NewBadge extends React.Component {
 
-   
+    state = {
+        loading:false,
+        error:null,
+        form:{
+            ProfilePic:"",
+            Name:"", 
+            Age:"",
+            City:"",
+            Followers:"",
+            Likes:"",
+            Imagenes:"",
+        }
+    }
+
+
     render() {
         return(
             <React.Fragment>
-                <h1>New Badge</h1>
+                <Hero h={"15vh"}>
+                    <div className="col">
+
+                    <div className="col"></div>
+                    </div>
+                </Hero>
+                <div className="container">
+                    <div className="col">
+                        <Badge
+                        ProfilePic = {this.state.form.ProfilePic || "https://images.pexels.com/photos/5372800/pexels-photo-5372800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                        ></Badge>
+                    </div>
+                    <div className="col">
+
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
