dfbd6d08c24edc9899192e055593faad1668e5ec
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 22:53:23 -0600

    Creating error 404 page

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import React from "react"
+import "./style/Error.css";
+import Error from "../images/404_error.png"
+const Error = () => {
+        return(
+            <React.Fragment>
+                <img src="../" alt="" />
+            </React.Fragment>
+        )
+    } 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Main.css";
 import Hero from "../../components/Hero";
-import Button from "../../components/Main_Button"
-const Main = () => {
+import Button from "../../components/Main_Button";
+const Main = (props) => {
     return (
         <React.Fragment>
             <div className="Main">
                 <div className="Main__content">
-                    <h1 className="Main__title">Welcome</h1>
-                    <Button contentText={"Hakiar"}></Button>
+                    <h1 className="Main__title">{props.title||"Welcome"}</h1>
+                    <Button contentText={props.button_text||"Hakiar"}></Button>
                 </div>
                 <Hero h={"88vh"}></Hero>
             </div>
