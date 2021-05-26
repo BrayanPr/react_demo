66dc38c9fedc4e3524efa35063cf1f8f9d80ab2e
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 21:38:57 -0600

    Creating form

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style/Form.css"
+
+class Form extends React.Component{
+     render(){
+         return(
+            <React.Fragment>
+                <h1>Form</h1>
+            </React.Fragment>
+         );
+     }
+}
+
+export default Form
--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from "../../components/Hero";
 import "./NewBadge.css";
 import Badge from "../../components/Badge"
+import Form from "../../components/Form"
 
 class NewBadge extends React.Component {
 
@@ -24,27 +25,25 @@ class NewBadge extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <Hero h={"15vh"}>
-                    <div className="col">
-
-                    <div className="col"></div>
-                    </div>
-                </Hero>
+                <Hero h={"15vh"}></Hero>
                 <div className="container">
-                    <div className="col">
-                        <Badge
-                        Header = {this.state.form.Header || "https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                        ProfilePic = {this.state.form.ProfilePic || "https://images.pexels.com/photos/5372800/pexels-photo-5372800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                        Name = {this.state.form.Name || "Maria"}
-                        Age = {this.state.form.Age || "22"}
-                        City = {this.state.form.City || "New York"}
-                        Followers = {this.state.form.Followers || "0"}
-                        Pictures = {this.state.form.Pictures || "0"}
-                        Likes = {this.state.form.Likes || "0"}
-                       ></Badge>
-                    </div>
-                    <div className="col">
-
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                            Header = {this.state.form.Header || "https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                            ProfilePic = {this.state.form.ProfilePic || "https://images.pexels.com/photos/5372800/pexels-photo-5372800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                            Name = {this.state.form.Name || "Maria"}
+                            Age = {this.state.form.Age || "22"}
+                            City = {this.state.form.City || "New York"}
+                            Followers = {this.state.form.Followers || "0"}
+                            Pictures = {this.state.form.Pictures || "0"}
+                            Likes = {this.state.form.Likes || "0"}>
+                                
+                            </Badge>
+                        </div>
+                        <div className="col">
+                            <Form></Form>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
