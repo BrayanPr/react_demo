a9a05c85a938c793981a3f2ab6633efff889a9c4
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-25 18:20:36 -0600

    creating new badge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react"
 import Layout from "./pages/Layout/Layout"
 import Main from "./pages/Main/Main"
-import Badge from "./components/Badge"
+import NewBadge from "../src/pages/NewBadge/NewBadge"
 
 function App() {
   return ( 
   <React.Fragment>
     <Layout>
-      <Main></Main>
-      <Badge
-      ProfilePic="https://images.pexels.com/photos/3891770/pexels-photo-3891770.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-      Name="Samanta Wong"
-      Age="24"
-      Followers="5k"
-      Likes="200k"
-      Pictures="324"
-      ></Badge>
+      <NewBadge></NewBadge>
     </Layout>
   </React.Fragment>  
   );
---
+++ src/pages/NewBadge/NewBadge.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from "../../components/Hero";
+import "./NewBadge.css";
+import Badge from "../../components/Badge"
+
+class NewBadge extends React.Component {
+
+   
+    render() {
+        return(
+            <React.Fragment>
+                <h1>New Badge</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default NewBadge;
