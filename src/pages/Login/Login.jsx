8a16b20f433efdff1315b085a0dce37666cc6c38
Brayan Prieto <a6519150032@utch.edu.mx>
2021-05-26 19:11:37 -0600

    Creando pagina login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React  from "react"
+import "./Login.css"
+import Layout from "../Layout/Layout"
+import Form from "../../components/Form"
+var Login = () =>{
+    return(
+        <Layout>
+        <Form></Form>
+        </Layout>
+    );
+    
+}
+
+export default Login
